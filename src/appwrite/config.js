cb8ec35f81113756c4fd0fc4d1b6544acab7e419
shaikh631 <as9251145@gmail.com>
2026-07-05 03:48:10 +0530

    making getPosts Boolean form

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -91,7 +91,7 @@ export class Service{
         }
     }
 
-    async getPosts(queries = [Query.equal("status", "active")]){
+   async getPosts(queries = [Query.equal("status", true)]) {
         try {
             return await this.databases.listDocuments(
                 conf.AppWriteDataBaseId,
